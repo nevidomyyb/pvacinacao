c68096f22b7239cd7c3863291428d1a175524366
Pedro Cunha <idk.pedroc@gmail.com>
2023-11-23 20:24:46 -0300

    endpoint de delete, put e filtro de proximidade no get

--- resources/ponto_vacinacao.py
+++ resources/ponto_vacinacao.py
@@ -3,8 +3,8 @@ from db import db
 from models import PontoVacinacao, Bairro
 from schemas import PontoVacinacaoGetSchema, PontoVacinacaoSchema
 from flask.views import MethodView
-from flask import request
-
+from flask import request, jsonify
+from sqlalchemy import or_
 
 blp = Blueprint('pontos_vacinacao', __name__, description= 'Operações de pontos de vacinação')
 
@@ -17,7 +17,18 @@ class Pontos(MethodView):
         if 'bairro' in args and args['bairro'] == None:
             return {}
         if 'bairro' in args and args.get('bairro'):
-            return PontoVacinacao.query.filter_by(bairro=args['bairro']).all()
+            if PontoVacinacao.query.filter_by(bairro=args['bairro']).count() == 0:
+                bairro_instancia = Bairro(args['bairro'])
+                b = Bairro.encontrar_proximos(bairro=bairro_instancia)
+                or_f = or_(PontoVacinacao.bairro == b["acima"]["instancia"].name, PontoVacinacao.bairro == b['abaixo']['instancia'].name)
+                pontos = PontoVacinacao.query.filter(or_f).all()
+                json_ = {
+                    "message": f"Não foram encontrados pontos de vacinação no bairro {bairro_instancia.value}, procurando também nos bairros próximos de acordo com a Região Administrativa: {b['acima']['instancia'].value} e {b['abaixo']['instancia'].value}. ",
+                    "pontos_de_vacinacao": PontoVacinacaoSchema(many=True).dump(pontos)
+                }
+                return jsonify(json_), 200
+            else:
+                return PontoVacinacao.query.filter_by(bairro=args['bairro']).all()
         else:
             return PontoVacinacao.query.all()
     
@@ -27,7 +38,7 @@ class Pontos(MethodView):
         ponto = PontoVacinacaoSchema.novo_ponto(ponto_data)
         return ponto
 
-@blp.route('/ponto/<uuid_>/')
+@blp.route('/ponto/<uuid:uuid_>/')
 class PontoUnico(MethodView):
 
     @blp.response(200, PontoVacinacaoSchema)
@@ -35,10 +46,29 @@ class PontoUnico(MethodView):
         ponto = PontoVacinacao.query.get_or_404(uuid_)
         return ponto
     
-    @blp.arguments(PontoVacinacaoSchema)
+    @blp.arguments(PontoVacinacaoSchema, location='json')
     @blp.response(204, PontoVacinacaoSchema)
-    def patch(self, uuid_):
+    def put(self, dados,uuid_):
         ponto = PontoVacinacao.query.get_or_404(uuid_)
+        try:
+            ponto_ = PontoVacinacaoSchema().update(ponto, dados)
+            db.session.add(ponto_)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+        
+        return ponto_
+    
+    blp.response(200)
+    def delete(self, uuid_):
+        ponto = PontoVacinacao.query.get_or_404(uuid_)
+        try:   
+            db.session.delete(ponto)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+        return {}
+
         
         
 
--- schemas/ponto_vacinacao.py
+++ schemas/ponto_vacinacao.py
@@ -5,14 +5,21 @@ from validators import ValorUnico, BairroValidador
 from db import db
 from flask_smorest import abort
 from sqlalchemy.exc import SQLAlchemyError
+from marshmallow.exceptions import ValidationError
 
-def validate_func(v):
-    print(v, "tte")
+def serialize(obj):
+    return Bairro.pegar_bairro(obj.bairro).value  if isinstance(obj, PontoVacinacao) else obj
+    
+def deserialize(obj):
+    return Bairro.pegar_bairro(obj)
 
 class PontoVacinacaoSchema(Schema):
     idpontovacinacao = fields.Str(dump_only=True)
     nome = fields.Str(required=True, validate=[ValorUnico(model=PontoVacinacao, campo='nome'), ])
-    bairro = fields.Function(validate=[BairroValidador(), ], serialize=lambda obj: Bairro.pegar_bairro(obj.bairro).value, deserialize= lambda value: Bairro.pegar_bairro(value))
+    bairro = fields.Function(
+        validate=[BairroValidador(), ], 
+        serialize=serialize, 
+        deserialize= lambda value: Bairro.pegar_bairro(value))
     endereco = fields.Str(required=True)
     horario_expediente = fields.Str(required=True)
     faixa_etaria = fields.Str(required=True)
@@ -29,3 +36,13 @@ class PontoVacinacaoSchema(Schema):
             print(e)
             abort(500, message="Ocorreu um erro interno ao salvar o ponto de vacinação.")
         return ponto_vacinacao
+
+    def update(self, obj, novos_dados):
+
+        campos_atualizaveis = [nome_campo for nome_campo, instancia_campo in  self.fields.items() if not instancia_campo.dump_only]
+        for campo in campos_atualizaveis:
+            setattr(obj, campo, novos_dados.get(campo))
+
+        return obj
+
+        
