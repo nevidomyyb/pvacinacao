b5fe2ad7b8b0b318d56c075f263e1d2a2bffc825
Pedro Cunha <idk.pedroc@gmail.com>
2023-11-24 03:22:14 -0300

    adicionando função para conectar no banco

--- app.py
+++ app.py
@@ -16,9 +16,12 @@ def create_app(db_url=None):
     DB_USER = os.environ.get("DB_USER")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_NAME = os.environ.get("DB_NAME")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DEBUG = os.environ.get("DEBUG")
 
     app.config["PROPAGATION_EXCEPTIONS"] = True
-    app.config["API_TITLE"] = "PROJETO INTEGRADOR REST API"
+    app.config["API_TITLE"] = "pVacinacao REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
@@ -26,7 +29,11 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{db_user}:{db_password}@localhost/{db_name}".format(db_user=DB_USER, db_password=DB_PASSWORD, db_name=DB_NAME)
+    if DEBUG == 1 or DEBUG == "1":
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{db_user}:{db_password}@localhost/{db_name}".format(db_user=DB_USER, db_password=DB_PASSWORD, db_name=DB_NAME)
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}".format(db_user=DB_USER, db_password=DB_PASSWORD, db_name=DB_NAME, db_host=DB_HOST, db_port=DB_PORT)
 
     db.init_app(app)
     migrate = Migrate(app, db)
